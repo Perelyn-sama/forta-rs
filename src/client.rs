877b597383f47c5638e4ff3e062b22857024cf36
Perelyn <pisckid344@gmail.com>
2023-03-19 18:39:00 +0100

    add client

---
+++ src/client.rs
@@ -0,0 +1,22 @@
+use agent::agent_client::AgentClient;
+use agent::InitializeRequest;
+
+pub mod agent {
+    tonic::include_proto!("network.forta");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = AgentClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(InitializeRequest {
+        agent_id: "0xagentid".to_owned(),
+        proxy_host: "".to_owned(),
+    });
+
+    let response = client.initialize(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
