3a3d9190de04d738c8968e1d3dee73935d914d4e
mahmudsudo <blinkztyler@gmail.com>
2023-05-21 14:01:03 +0100

    constructor fix

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,14 @@
+use std::collections::HashMap;
 use tonic::{transport::Server, Request, Response, Status};
-use promises::Promise;
 
 use agent::agent_server::{Agent, AgentServer};
 use agent::{
-    EvaluateAlertRequest, EvaluateAlertResponse, EvaluateBlockRequest, EvaluateBlockResponse,
-    EvaluateTxRequest, EvaluateTxResponse, InitializeRequest, InitializeResponse,TransactionEvent,Finding,BlockEvent,AlertEvent
+    AlertEvent, BlockEvent, EvaluateAlertRequest, EvaluateAlertResponse, EvaluateBlockRequest,
+    EvaluateBlockResponse, EvaluateTxRequest, EvaluateTxResponse, Finding, InitializeRequest,
+    InitializeResponse, ResponseStatus, TransactionEvent,
 };
+// use agent
+use std::future::Future;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -15,82 +18,80 @@ pub mod agent {
 
 #[derive(Debug)]
 pub struct AgentService {
+    get_agent_handlers: Option<Arc<Mutex<GetAgentHandlers>>>,
     is_initialized: Arc<Mutex<bool>>,
-    get_agent_handlers : Option<GetAgentHandlers>,
-    initialize_response : Option<InitializeResponse>
+    initialize_response: Option<Arc<Mutex<InitializeResponse>>>,
 }
+impl AgentService {
+    fn new(get_agent_handlers: Option<Arc<Mutex<GetAgentHandlers>>>) -> Self {
+        assert!(
+            get_agent_handlers.is_some(),
+            "get_agent_handlers must exist"
+        );
+        let mut agent = AgentService {
+            get_agent_handlers,
+            is_initialized: Arc::new(Mutex::new(false)),
+            initialize_response: Some(Arc::new(Mutex::new(InitializeResponse::default()))),
+        };
 
+        Self::initialize_agent_handlers();
+
+        agent
+    }
+
+    fn initialize_agent_handlers() {
+        todo!()
+    }
+}
 impl Default for AgentService {
     fn default() -> Self {
         AgentService {
             is_initialized: Arc::new(Mutex::new(false)),
-            get_agent_handlers : None,
-            initialize_response : None
+            get_agent_handlers: None,
+            initialize_response: None,
         }
     }
 }
- struct  GetAgentHandlersOptions {shouldRunInitialize: Option<bool>}
-type GetAgentHandlers = fn(options : Option<GetAgentHandlersOptions>) -> Promise<AgentHandlers,Option<AgentHandlers>>;
-//export type Initialize = () => Promise<InitializeResponse | void>
-type Initialize =  fn() -> Promise<InitializeResponse,Option<InitializeResponse>>;
 
-//export type HandleBlock = (blockEvent: BlockEvent) => Promise<Finding[]>
-type HandleBlock = fn(blockEvent :BlockEvent) -> Promise<Vec<Finding>, Option<Finding>>;
-//export type HandleAlert = (alertEvent: AlertEvent) => Promise<Finding[]>
-type  HandleAlert = fn(alertEvent: AlertEvent) -> Promise<Vec<Finding>, Option<Finding>>;
-//export type HandleTransaction = (txEvent: TransactionEvent) => Promise<Finding[]>
-type HandleTransaction = fn (txEvent: TransactionEvent) -> Promise<Vec<Finding>,Option<Finding>>;
+#[derive(Debug)]
+struct GetAgentHandlersOptions {
+    should_run_initialize: Option<bool>,
+}
+type GetAgentHandlers =
+    fn(options: Option<GetAgentHandlersOptions>) -> Box<dyn Future<Output = AgentHandlers> + 'static>;
+type Initialize = fn() -> Box<dyn Future<Output = Option<InitializeResponse>> + 'static>;
+type HandleBlock = fn(blockEvent: BlockEvent) -> Box<dyn Future<Output = Vec<Finding>> + 'static>;
+type HandleAlert = fn(alertEvent: AlertEvent) -> Box<dyn Future<Output = Vec<Finding>> + 'static>;
+type HandleTransaction = fn(txEvent: TransactionEvent) -> Box<dyn Future<Output = Vec<Finding>> + 'static>;
 
-
-struct  AgentHandlers {
+#[derive(Debug)]
+struct AgentHandlers {
     initialize: Initialize,
     initialize_response: InitializeResponse,
     handle_transaction: HandleTransaction,
     handle_block: HandleBlock,
     handle_alert: HandleAlert,
 }
-//constructor(getAgentHandlers) {
-//    assertExists(getAgentHandlers, "getAgentHandlers");
-//    this.getAgentHandlers = getAgentHandlers;
-//    this.initializeAgentHandlers();
-//    this.isInitialized = false; // makes sure agent initialize handler only called once
-//      this.initializeResponse = {};
-//}
-impl AgentService{
 
-    fn initialize_agent_handlers() {
-        todo!()
-    }
-
-    fn build(get_agent_handlers : Option<GetAgentHandlers>) ->Self{
-        Self::initialize_agent_handlers();
-        Self { is_initialized: Arc::new(Mutex::new(false)), get_agent_handlers , initialize_response: None }
-    }
-
-}
 #[tonic::async_trait]
 impl Agent for AgentService {
     async fn initialize(
         &self,
         request: Request<InitializeRequest>,
-    ) -> Result<Response<InitializeResponse>, Status> {
+        ) -> Result<Response<InitializeResponse>, Status> {
         println!("Got a request: {:?}", request);
 
         let mut is_initialized = self.is_initialized.lock().await;
         *is_initialized = true;
 
         let req = request.into_inner();
-        // dbg!(&req);
-        // dbg!(&req.agent_id);
 
-        // FIXME remove hardcoded values
         let reply = InitializeResponse {
-            status: Status::ok("").code().into(),
+            status: ResponseStatus::Success as i32,
             errors: vec![agent::Error {
                 message: String::default(),
             }],
             addresses: vec![String::default()],
-            // alert_config: Some(agent::AlertConfig::default()),
             alert_config: Some(agent::AlertConfig {
                 subscriptions: vec![agent::CombinerBotSubscription {
                     bot_id: req.agent_id,
@@ -106,24 +107,36 @@ impl Agent for AgentService {
     async fn evaluate_tx(
         &self,
         request: Request<EvaluateTxRequest>,
-    ) -> Result<Response<EvaluateTxResponse>, Status> {
-        unimplemented!()
-//        println!("evaluate tx request received : {:?}",request);
-//        let req = request.into_inner();
+        ) -> Result<Response<EvaluateTxResponse>, Status> {
+        println!("Got a request: {:?}", request);
 
+        let req = request.into_inner();
+        dbg!(req);
 
+        let reply = EvaluateTxResponse {
+            status: ResponseStatus::Success as i32,
+            errors: vec![agent::Error {
+                message: String::default(),
+            }],
+            findings: vec![],
+            metadata: HashMap::default(),
+            timestamp: String::new(),
+            latency_ms: u32::default(),
+            private: false,
+        };
 
+        Ok(Response::new(reply))
     }
     async fn evaluate_block(
         &self,
         request: Request<EvaluateBlockRequest>,
-    ) -> Result<Response<EvaluateBlockResponse>, Status> {
+        ) -> Result<Response<EvaluateBlockResponse>, Status> {
         unimplemented!()
     }
     async fn evaluate_alert(
         &self,
         request: Request<EvaluateAlertRequest>,
-    ) -> Result<Response<EvaluateAlertResponse>, Status> {
+        ) -> Result<Response<EvaluateAlertResponse>, Status> {
         unimplemented!()
     }
 }
@@ -131,6 +144,7 @@ impl Agent for AgentService {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
+    // let agent_service = AgentService::default();
     let agent_service = AgentService::default();
 
     Server::builder()
@@ -139,4 +153,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     Ok(())
 }
-//
