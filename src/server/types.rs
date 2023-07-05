12d90021371671b9e38a1605e69c5c5ec287f5cd
Perelyn <pisckid344@gmail.com>
2023-07-05 23:45:37 +0100

    add functions to AgentHandlers impl

--- src/server/types.rs
+++ src/server/types.rs
@@ -11,7 +11,7 @@ pub(crate) struct GetAgentHandlersOptions {
 }
 pub(crate) type GetAgentHandlers = fn(
     options: Option<GetAgentHandlersOptions>,
-) -> Box<dyn Future<Output = AgentHandlers> + 'static>;
+) -> Box<dyn Future<Output = AgentHandlers> + 'static + Unpin>;
 pub(crate) type Initialize = fn() -> Box<dyn Future<Output = Option<InitializeResponse>> + 'static>;
 pub(crate) type HandleBlock =
     fn(blockEvent: BlockEvent) -> Box<dyn Future<Output = Vec<Finding>> + 'static>;
@@ -22,9 +22,35 @@ pub(crate) type HandleTransaction =
 
 #[derive(Debug)]
 pub(crate) struct AgentHandlers {
-    initialize: Initialize,
-    initialize_response: InitializeResponse,
-    handle_transaction: HandleTransaction,
-    handle_block: HandleBlock,
-    handle_alert: HandleAlert,
+    initialize: Option<Initialize>,
+    initialize_response: Option<InitializeResponse>,
+    handle_transaction: Option<HandleTransaction>,
+    handle_block: Option<HandleBlock>,
+    handle_alert: Option<HandleAlert>,
+}
+
+impl AgentHandlers {
+    async fn initialize_agent_handlers(&mut self) {
+        let options = GetAgentHandlersOptions {
+            should_run_initialize: Some(false),
+        };
+        let agent_handlers = Box::pin(Self::get_agent_handlers(Some(options))).await;
+
+        self.initialize = agent_handlers.initialize;
+        self.initialize_response = agent_handlers.initialize_response;
+        self.handle_transaction = agent_handlers.handle_transaction;
+        self.handle_block = agent_handlers.handle_block;
+        self.handle_alert = agent_handlers.handle_alert;
+    }
+
+    // Implement the function with the specified signature
+    fn get_agent_handlers(
+        options: Option<GetAgentHandlersOptions>,
+    ) -> Box<dyn Future<Output = AgentHandlers> + 'static + Unpin> {
+        // Function implementation goes here
+        // You can create and return a future that resolves to an AgentHandlers instance
+        // For simplicity, we'll just return an empty Boxed future here
+        // Box::pin(async move { AgentHandlers {} })
+        unimplemented!()
+    }
 }
